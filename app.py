62cdae392b049a0b918fa7ca77b91468f4da614d
erikhalt <erikhalt@hotmail.com>
2023-03-18 19:32:33 +0100

    updated with grid

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import pygame,sys
+import pygame,sys,pygame_textinput
 from config import *
 
 
@@ -10,65 +10,118 @@ class GUI():
         self.screen = pygame.display.set_mode((widht,height))
         self.clock = pygame.time.Clock()
         self.rectlist = []
-        pygame.display.set_caption('Pathfinder')
-        self.creategrid()
         self.startrect = None
         self.choosestart = False
         self.chooseend = False
         self.endrect = None
-
-    def creategrid(self):
-        for row_index in range(0,int(height/tilesize)-10,1):
-            for col_index in range(0,int(widht/tilesize),1):
-                color = 'white'
-                rect = pygame.Rect(col_index*tilesize,row_index*tilesize,tilesize-1,tilesize-1)
-                self.rectlist.append((rect,color))
-
-    def drawgrid(self):
-        for rect,color in self.rectlist:
-            pygame.draw.rect(self.screen,color,rect)
+        pygame.display.set_caption('pathfinding')
+        
+        self.grid = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,3,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,3,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,3,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,3,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,3,3,3,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,2,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],]
+        
+        self.creategrid()
     
     def run(self):
         while True:
             self.clock.tick(fps)
-            pygame.display.update()
+            pygame.display.flip()
+            self.drawgrid()        
+            self.updategrid()
             
-            self.drawgrid()
-
             for event in pygame.event.get():
                 key = pygame.key.get_pressed()
+
                 if event.type == pygame.QUIT:
                     sys.exit()
+
                 if key[pygame.K_r]:
                     self.choosestart = True
+
                 if key[pygame.K_t]:
                     self.chooseend = True
+
                 if key[pygame.K_q]:
                     self.startrect = None
                     self.choosestart = False
                     self.chooseend = False
                     self.endrect = None
-                    for object, (rect,color) in enumerate(self.rectlist):
-                        self.rectlist[object] = (rect,'white')
+                    for object, (rect,color,pos) in enumerate(self.rectlist):
+                        self.rectlist[object] = (rect,'white',pos)
                 
                 if event.type == pygame.MOUSEBUTTONUP:
 
                     mouseposition = pygame.mouse.get_pos()
-                    for object, (rect,color) in enumerate(self.rectlist):
+                    for object, (rect,color,pos) in enumerate(self.rectlist):
                         if rect.collidepoint(mouseposition):
                             if self.choosestart:
-                                self.rectlist[object] = (rect,'green')
+                                self.rectlist[object] = (rect,'green',pos)
                                 self.startrect = self.rectlist[object]
                                 self.choosestart = False
                             elif self.chooseend:
-                                self.rectlist[object] = (rect,'red')
+                                self.rectlist[object] = (rect,'red',pos)
                                 self.endrect = self.rectlist[object]
                                 self.chooseend = False
                             else:
-                                self.rectlist[object] = (rect,'blue')
-            
+                                self.rectlist[object] = (rect,'blue',pos)
+                
+                if key[pygame.K_SPACE]:
+                    for row in self.grid:
+                        print(row)
 
-    
+
+    def creategrid(self):
+        for row_index, row in enumerate(self.grid):
+            for col_index, col in enumerate(row):
+                if col == 0:
+                    rect = pygame.Rect((col_index*tilesize+((widht-(len(self.grid[1])*tilesize))/2),row_index*tilesize,tilesize-1,tilesize-1))
+                    color = 'white'
+                    pos = row_index,col_index
+                    self.rectlist.append((rect,color,pos))
+                if col == 1:
+                    rect = pygame.Rect((col_index*tilesize+((widht-(len(self.grid[1])*tilesize))/2),row_index*tilesize,tilesize-1,tilesize-1))
+                    color = 'green'
+                    pos = row_index,col_index
+                    self.rectlist.append((rect,color,pos))
+                if col == 2:
+                    rect = pygame.Rect((col_index*tilesize+((widht-(len(self.grid[1])*tilesize))/2),row_index*tilesize,tilesize-1,tilesize-1))
+                    color = 'red'
+                    pos = row_index,col_index
+                    self.rectlist.append((rect,color,pos))
+                if col == 3:
+                    rect = pygame.Rect((col_index*tilesize+((widht-(len(self.grid[1])*tilesize))/2),row_index*tilesize,tilesize-1,tilesize-1))
+                    color = 'blue'
+                    pos = row_index,col_index
+                    self.rectlist.append((rect,color,pos))
+
+
+    def drawgrid(self):
+        for rect,color,pos in self.rectlist:
+            pygame.draw.rect(self.screen,color,rect)
+
+    def updategrid(self):
+        for rect,color,pos in self.rectlist:
+            row_index,col_index = pos
+            if color == 'white':
+                self.grid[row_index][col_index] = 0
+            if color == 'green':
+                self.grid[row_index][col_index] = 1
+            if color == 'red':
+                self.grid[row_index][col_index] = 2
+            if color == 'blue':
+                self.grid[row_index][col_index] = 3
 
 if __name__ is "__main__":
     gui = GUI()
